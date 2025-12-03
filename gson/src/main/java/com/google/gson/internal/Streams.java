2ec982b31fc8bd29eb5d5c71b72079377e06ee7c
Pascal-Joos <pascal.joos@outlook.de>
2025-12-03 13:59:49 +0000

    fix: 27 - FIELD_NO_INIT - char[] chars; - @NonNull field Streams$AppendableWriter$CurrentWrite.chars not initialized

--- gson/src/main/java/com/google/gson/internal/Streams.java
+++ gson/src/main/java/com/google/gson/internal/Streams.java
@@ -98,7 +98,7 @@ public final class Streams {
 
     /** A mutable char sequence pointing at a single char[]. */
     static class CurrentWrite implements CharSequence {
-      char[] chars;
+      char[] chars = new char[0];
 
       public int length() {
         return chars.length;

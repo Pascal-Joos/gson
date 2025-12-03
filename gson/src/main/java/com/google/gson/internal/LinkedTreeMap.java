02af4e7d877d8a83b8c74dbabe75a39d7416f2e0
Pascal-Joos <pascal.joos@outlook.de>
2025-12-03 13:53:15 +0000

    Revert "fix: 23 - DEREFERENCE_NULLABLE - Node<K, V> rightLeft = right.left; - dereferenced expression right is @Nullable"
    This reverts commit 8ae561e886117a6c77bd10eeb0b4829d657d2439.

--- gson/src/main/java/com/google/gson/internal/LinkedTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedTreeMap.java
@@ -325,25 +325,19 @@ public final class LinkedTreeMap<K, V> extends AbstractMap<K, V> implements Seri
 
       int delta = leftHeight - rightHeight;
       if (delta == -2) {
-        assert right != null;
-        Node<K, V> rightNode = right;
-        Node<K, V> rightLeft = rightNode.left;
-        Node<K, V> rightRight = rightNode.right;
+        Node<K, V> rightLeft = right.left;
+        Node<K, V> rightRight = right.right;
         int rightRightHeight = rightRight != null ? rightRight.height : 0;
         int rightLeftHeight = rightLeft != null ? rightLeft.height : 0;
+
         int rightDelta = rightLeftHeight - rightRightHeight;
         if (rightDelta == -1 || (rightDelta == 0 && !insert)) {
           rotateLeft(node); // AVL right right
         } else {
           assert (rightDelta == 1);
-          rotateRight(rightNode); // AVL right left
+          rotateRight(right); // AVL right left
           rotateLeft(node);
         }
-        if (insert) {
-          break; // no further rotations will be necessary
-        }
-      } else if (delta == 2) {
-
         if (insert) {
           break; // no further rotations will be necessary
         }

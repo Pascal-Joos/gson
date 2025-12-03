df1ef60cc9c04877ac6c362b90cb05ca33fbbb93
Pascal-Joos <pascal.joos@outlook.de>
2025-12-03 13:31:38 +0000

    Revert "fix: 12 - DEREFERENCE_NULLABLE - Node<K, V> pivotLeft = pivot.left; - dereferenced expression pivot is @Nullable"
    This reverts commit 747017adb0eed380825e95e5dc86ad06e0067562.

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -406,9 +406,6 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
   private void rotateLeft(Node<K, V> root) {
     Node<K, V> left = root.left;
     Node<K, V> pivot = root.right;
-    if (pivot == null) {
-      return;
-    }
     Node<K, V> pivotLeft = pivot.left;
     Node<K, V> pivotRight = pivot.right;
 

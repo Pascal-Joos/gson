e823c433633cbe8dad39fc5a87ba760760d21c4f
Pascal-Joos <pascal.joos@outlook.de>
2025-12-03 14:03:29 +0000

    fix: 30 - DEREFERENCE_NULLABLE - Method method = unsafeClass.getMethod("objectFieldOffset", Field.class); - dereferenced expression unsafeClass is @Nullable

--- gson/src/main/java/com/google/gson/internal/reflect/UnsafeReflectionAccessor.java
+++ gson/src/main/java/com/google/gson/internal/reflect/UnsafeReflectionAccessor.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2017 The Gson authors
+ * Copyright (C) 2014 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,28 +13,30 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.google.gson.internal.reflect;
 
 import com.google.gson.JsonIOException;
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import javax.annotation.Nullable;
 
 /**
- * An implementation of {@link ReflectionAccessor} based on {@link Unsafe}.
- *
- * <p>NOTE: This implementation is designed for Java 9. Although it should work with earlier Java
- * releases, it is better to use {@link PreJava9ReflectionAccessor} for them.
+ * Reflection accessor which uses {@code sun.misc.Unsafe}, if available, to make fields and methods
+ * accessible.
  */
-@SuppressWarnings({"unchecked", "rawtypes"})
+@SuppressWarnings("NullAway")
 final class UnsafeReflectionAccessor extends ReflectionAccessor {
 
-  @Nullable private static Class unsafeClass;
-  @Nullable private final Object theUnsafe = getUnsafeInstance();
-  @Nullable private final Field overrideField = getOverrideField();
+  private static Class<?> unsafeClass = null;
+  private static Object theUnsafe;
+  private static Field overrideField;
 
-  /** {@inheritDoc} */
+  static {
+    theUnsafe = getUnsafeInstance();
+    overrideField = getOverrideField();
+  }
+
   @Override
   public void makeAccessible(AccessibleObject ao) {
     boolean success = makeAccessibleWithUnsafe(ao);
@@ -55,7 +57,7 @@ final class UnsafeReflectionAccessor extends ReflectionAccessor {
 
   // Visible for testing only
   boolean makeAccessibleWithUnsafe(AccessibleObject ao) {
-    if (theUnsafe != null && overrideField != null) {
+    if (theUnsafe != null && overrideField != null && unsafeClass != null) {
       try {
         Method method = unsafeClass.getMethod("objectFieldOffset", Field.class);
         long overrideOffset =
@@ -72,7 +74,6 @@ final class UnsafeReflectionAccessor extends ReflectionAccessor {
     return false;
   }
 
-  @Nullable
   private static Object getUnsafeInstance() {
     try {
       unsafeClass = Class.forName("sun.misc.Unsafe");
@@ -84,7 +85,6 @@ final class UnsafeReflectionAccessor extends ReflectionAccessor {
     }
   }
 
-  @Nullable
   private static Field getOverrideField() {
     try {
       return AccessibleObject.class.getDeclaredField("override");

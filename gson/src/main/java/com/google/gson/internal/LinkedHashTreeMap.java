80e1d3a886d9f355133c8b0c70d302531d4b5980
Pascal-Joos <pascal.joos@outlook.de>
2025-12-08 12:22:20 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression center is @Nullable' /home/vscode/nullness-benchmarks/gson/gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java:729           Node<K, V> left = center.parent;

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -743,6 +743,9 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
         if (leavesSkipped == 0) {
           // Pop right, center and left, then make center the top of the stack.
           Node<K, V> right = stack;
+          if (right == null) {
+            throw new IllegalStateException();
+          }
           Node<K, V> center = right.parent;
           Node<K, V> left = center.parent;
           center.parent = left.parent;
@@ -756,6 +759,9 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
         } else if (leavesSkipped == 1) {
           // Pop right and center, then make center the top of the stack.
           Node<K, V> right = stack;
+          if (right == null) {
+            throw new IllegalStateException();
+          }
           Node<K, V> center = right.parent;
           stack = center;
           // Construct a tree with no left child.

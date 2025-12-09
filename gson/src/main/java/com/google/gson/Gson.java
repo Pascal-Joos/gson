2205c08efab485d3bdeb5529d690772f5127b684
Pascal-Joos <pascal.joos@outlook.de>
2025-12-09 15:06:23 +0000

    fix: 1 - DEREFERENCE_NULLABLE - return new AtomicLong(value.longValue()); - dereferenced expression value is @Nullable

--- gson/src/main/java/com/google/gson/Gson.java
+++ gson/src/main/java/com/google/gson/Gson.java
@@ -431,7 +431,7 @@ public final class Gson {
       @Override
       public AtomicLong read(JsonReader in) throws IOException {
         Number value = longAdapter.read(in);
-        return new AtomicLong(value.longValue());
+        return value == null ? null : new AtomicLong(value.longValue());
       }
     }.nullSafe();
   }

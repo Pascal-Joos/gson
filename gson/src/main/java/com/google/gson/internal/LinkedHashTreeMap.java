85232aa8404665053f05fda95603b06bd69e3118
Pascal-Joos <pascal.joos@outlook.de>
2025-12-09 16:37:11 +0000

    Revert "fix: 18 - DEREFERENCE_NULLABLE - Node<K, V> center = right.parent; - dereferenced expression right is @Nullable"
    This reverts commit b7191a40f74dfdc967c6bdc81e3c75e53f0bdf3d.

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -723,19 +723,10 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
        */
       for (int scale = 4; (size & scale - 1) == scale - 1; scale *= 2) {
         if (leavesSkipped == 0) {
-          Node<K, V> right = stack;
-          if (right == null || right.parent == null || right.parent.parent == null) {
-            throw new IllegalStateException();
-          }
           // Pop right, center and left, then make center the top of the stack.
+          Node<K, V> right = stack;
           Node<K, V> center = right.parent;
-          if (center == null || center.parent == null) {
-            throw new IllegalStateException();
-          }
           Node<K, V> left = center.parent;
-          if (left == null) {
-            throw new IllegalStateException();
-          }
           center.parent = left.parent;
           stack = center;
           // Construct a tree.
@@ -747,13 +738,7 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
         } else if (leavesSkipped == 1) {
           // Pop right and center, then make center the top of the stack.
           Node<K, V> right = stack;
-          if (right == null || right.parent == null) {
-            throw new IllegalStateException();
-          }
           Node<K, V> center = right.parent;
-          if (center == null) {
-            throw new IllegalStateException();
-          }
           stack = center;
           // Construct a tree with no left child.
           center.right = right;
@@ -768,7 +753,7 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
 
     Node<K, V> root() {
       Node<K, V> stackTop = this.stack;
-      if (stackTop == null || stackTop.parent != null) {
+      if (stackTop.parent != null) {
         throw new IllegalStateException();
       }
       return stackTop;

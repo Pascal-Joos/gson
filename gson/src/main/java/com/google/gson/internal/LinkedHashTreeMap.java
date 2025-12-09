dff918a19fb28859f537e341445ce9aa750e0d20
Pascal-Joos <pascal.joos@outlook.de>
2025-12-09 16:34:30 +0000

    Revert "fix: 16 - DEREFERENCE_NULLABLE - center.parent = left.parent; - dereferenced expression left is @Nullable"
    This reverts commit 57bf3a815a7df1a3665d4e76b2f157e367bf1eea.

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -493,12 +493,8 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
       this.height = 1;
       this.next = next;
       this.prev = prev;
-      if (prev != null) {
-        prev.next = this;
-      }
-      if (next != null) {
-        next.prev = this;
-      }
+      prev.next = this;
+      next.prev = this;
     }
 
     @Nullable
@@ -727,20 +723,11 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
        */
       for (int scale = 4; (size & scale - 1) == scale - 1; scale *= 2) {
         if (leavesSkipped == 0) {
+          // Pop right, center and left, then make center the top of the stack.
           Node<K, V> right = stack;
-          if (right == null) {
-            break;
-          }
           Node<K, V> center = right.parent;
-          if (center == null) {
-            break;
-          }
           Node<K, V> left = center.parent;
-          if (left == null) {
-            break;
-          }
-          Node<K, V> parent = left.parent;
-          center.parent = parent;
+          center.parent = left.parent;
           stack = center;
           // Construct a tree.
           center.left = left;
@@ -749,14 +736,9 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
           left.parent = center;
           right.parent = center;
         } else if (leavesSkipped == 1) {
+          // Pop right and center, then make center the top of the stack.
           Node<K, V> right = stack;
-          if (right == null) {
-            break;
-          }
           Node<K, V> center = right.parent;
-          if (center == null) {
-            break;
-          }
           stack = center;
           // Construct a tree with no left child.
           center.right = right;
@@ -771,7 +753,7 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
 
     Node<K, V> root() {
       Node<K, V> stackTop = this.stack;
-      if (stackTop == null || stackTop.parent != null) {
+      if (stackTop.parent != null) {
         throw new IllegalStateException();
       }
       return stackTop;

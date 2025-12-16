d5d5efe891f155cdcd806e1f0f1cf6ff3eecbffc
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 14:38:52 +0000

    fix: 11 - DEREFERENCE_NULLABLE - node.prev.next = node.next; - dereferenced expression node.prev is @Nullable

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -251,8 +251,14 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
    */
   void removeInternal(Node<K, V> node, boolean unlink) {
     if (unlink) {
-      node.prev.next = node.next;
-      node.next.prev = node.prev;
+      Node<K, V> prev = node.prev;
+      Node<K, V> next = node.next;
+      if (prev != null) {
+        prev.next = next;
+      }
+      if (next != null) {
+        next.prev = prev;
+      }
       node.next = node.prev = null; // Help the GC (for performance)
     }
 

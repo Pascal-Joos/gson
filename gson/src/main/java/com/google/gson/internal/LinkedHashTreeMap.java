ff13689757bef65ca6b1f19c2ef6e4d8e1b8d936
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 14:37:31 +0000

    fix: 8 - DEREFERENCE_NULLABLE - Node<K, V> leftLeft = left.left; - dereferenced expression left is @Nullable

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -351,6 +351,9 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
 
       int delta = leftHeight - rightHeight;
       if (delta == -2) {
+        if (right == null) {
+          break;
+        }
         Node<K, V> rightLeft = right.left;
         Node<K, V> rightRight = right.right;
         int rightRightHeight = rightRight != null ? rightRight.height : 0;
@@ -369,6 +372,9 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
         }
 
       } else if (delta == 2) {
+        if (left == null) {
+          break;
+        }
         Node<K, V> leftLeft = left.left;
         Node<K, V> leftRight = left.right;
         int leftRightHeight = leftRight != null ? leftRight.height : 0;

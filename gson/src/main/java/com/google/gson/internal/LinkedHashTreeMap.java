ab9bf86be2e0424c810a0ea83b5bd6f91997b742
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 14:40:50 +0000

    fix: 15 - DEREFERENCE_NULLABLE - center.right = right; - dereferenced expression center is @Nullable

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -726,7 +726,13 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
           // Pop right, center and left, then make center the top of the stack.
           Node<K, V> right = stack;
           Node<K, V> center = right.parent;
+          if (center == null) {
+            break;
+          }
           Node<K, V> left = center.parent;
+          if (left == null) {
+            break;
+          }
           center.parent = left.parent;
           stack = center;
           // Construct a tree.
@@ -739,6 +745,9 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
           // Pop right and center, then make center the top of the stack.
           Node<K, V> right = stack;
           Node<K, V> center = right.parent;
+          if (center == null) {
+            break;
+          }
           stack = center;
           // Construct a tree with no left child.
           center.right = right;

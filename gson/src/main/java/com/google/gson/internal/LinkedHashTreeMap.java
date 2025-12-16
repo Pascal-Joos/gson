bc55a9f815602bdf6c1b6ddf6e58aa89e4991210
Pascal-Joos <pascal.joos@outlook.de>
2025-12-16 14:42:23 +0000

    Revert "fix: 17 - DEREFERENCE_NULLABLE - Node<K, V> center = right.parent; - dereferenced expression right is @Nullable"
    This reverts commit 90395bba1233de99e92849534382eaa93815d09c.

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -723,26 +723,24 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
        */
       for (int scale = 4; (size & scale - 1) == scale - 1; scale *= 2) {
         if (leavesSkipped == 0) {
+          // Pop right, center and left, then make center the top of the stack.
           Node<K, V> right = stack;
-          if (right.parent == null || right.parent.parent == null) {
-            break;
-          }
           Node<K, V> center = right.parent;
           Node<K, V> left = center.parent;
           center.parent = left.parent;
           stack = center;
+          // Construct a tree.
           center.left = left;
           center.right = right;
           center.height = right.height + 1;
           left.parent = center;
           right.parent = center;
         } else if (leavesSkipped == 1) {
+          // Pop right and center, then make center the top of the stack.
           Node<K, V> right = stack;
-          if (right.parent == null) {
-            break;
-          }
           Node<K, V> center = right.parent;
           stack = center;
+          // Construct a tree with no left child.
           center.right = right;
           center.height = right.height + 1;
           right.parent = center;

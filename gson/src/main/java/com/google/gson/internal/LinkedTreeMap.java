3b56657af697965d60111b163ecf547ba56cd7a0
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 12:18:41 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression left is @Nullable' /home/vscode/nullness-benchmarks/gson/gson/src/main/java/com/google/gson/internal/LinkedTreeMap.java:346         Node<K, V> leftLeft = left.left;

--- gson/src/main/java/com/google/gson/internal/LinkedTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedTreeMap.java
@@ -328,6 +328,9 @@ public final class LinkedTreeMap<K, V> extends AbstractMap<K, V> implements Seri
 
       int delta = leftHeight - rightHeight;
       if (delta == -2) {
+        if (right == null) {
+          break;
+        }
         Node<K, V> rightLeft = right.left;
         Node<K, V> rightRight = right.right;
         int rightRightHeight = rightRight != null ? rightRight.height : 0;
@@ -346,6 +349,9 @@ public final class LinkedTreeMap<K, V> extends AbstractMap<K, V> implements Seri
         }
 
       } else if (delta == 2) {
+        if (left == null) {
+          break;
+        }
         Node<K, V> leftLeft = left.left;
         Node<K, V> leftRight = left.right;
         int leftRightHeight = leftRight != null ? leftRight.height : 0;

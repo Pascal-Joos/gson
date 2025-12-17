9fa09fb648ea82c7524d360c2b8cae5091afbd73
Pascal-Joos <pascal.joos@outlook.de>
2025-12-17 12:18:07 +0000

    fix: Type='DEREFERENCE_NULLABLE', message='dereferenced expression prev is @Nullable' /home/vscode/nullness-benchmarks/gson/gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java:496       prev.next = this;

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -508,6 +508,7 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
     }
 
     /** Create a regular entry */
+    /** Create a regular entry */
     Node(Node<K, V> parent, K key, int hash, Node<K, V> next, @Nullable Node<K, V> prev) {
       this.parent = parent;
       this.key = key;
@@ -515,8 +516,12 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
       this.height = 1;
       this.next = next;
       this.prev = prev;
-      prev.next = this;
-      next.prev = this;
+      if (prev != null) {
+        prev.next = this;
+      }
+      if (next != null) {
+        next.prev = this;
+      }
     }
 
     @Nullable

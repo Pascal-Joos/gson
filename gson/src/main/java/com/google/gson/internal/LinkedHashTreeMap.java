8ed9c0e7a3acc0236e4ed68cb7d73a43c522d796
Pascal-Joos <pascal.joos@outlook.de>
2025-12-19 10:59:53 +0000

    fix: 8 - DEREFERENCE_NULLABLE - Node<K, V> leftLeft = left.left; - dereferenced expression left is @Nullable

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -725,25 +725,49 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
         if (leavesSkipped == 0) {
           // Pop right, center and left, then make center the top of the stack.
           Node<K, V> right = stack;
+          if (right == null) {
+            break;
+          }
           Node<K, V> center = right.parent;
+          if (center == null) {
+            break;
+          }
           Node<K, V> left = center.parent;
-          center.parent = left.parent;
+          if (left == null) {
+            break;
+          }
+          Node<K, V> parent = left.parent;
+          center.parent = parent;
           stack = center;
           // Construct a tree.
           center.left = left;
-          center.right = right;
-          center.height = right.height + 1;
+          if (right != null) {
+            center.right = right;
+          }
+          center.height = (right != null ? right.height : 0) + 1;
           left.parent = center;
-          right.parent = center;
+          if (right != null) {
+            right.parent = center;
+          }
         } else if (leavesSkipped == 1) {
           // Pop right and center, then make center the top of the stack.
           Node<K, V> right = stack;
+          if (right == null) {
+            break;
+          }
           Node<K, V> center = right.parent;
+          if (center == null) {
+            break;
+          }
           stack = center;
           // Construct a tree with no left child.
-          center.right = right;
-          center.height = right.height + 1;
-          right.parent = center;
+          if (right != null) {
+            center.right = right;
+          }
+          center.height = (right != null ? right.height : 0) + 1;
+          if (right != null) {
+            right.parent = center;
+          }
           leavesSkipped = 0;
         } else if (leavesSkipped == 2) {
           leavesSkipped = 0;

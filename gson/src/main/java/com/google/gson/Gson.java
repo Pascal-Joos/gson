8ff205c4e4f5ef882efd646dd3d6b0eaec5f44d6
Pascal-Joos <pascal.joos@outlook.de>
2025-12-19 13:39:43 +0000

    fix: 2 - DEREFERENCE_NULLABLE - long value = longAdapter.read(in).longValue(); - dereferenced expression longAdapter.read(in) is @Nullable

--- gson/src/main/java/com/google/gson/Gson.java
+++ gson/src/main/java/com/google/gson/Gson.java
@@ -453,7 +453,11 @@ public final class Gson {
         List<Long> list = new ArrayList<Long>();
         in.beginArray();
         while (in.hasNext()) {
-          long value = longAdapter.read(in).longValue();
+          Number number = longAdapter.read(in);
+          if (number == null) {
+            continue;
+          }
+          long value = number.longValue();
           list.add(value);
         }
         in.endArray();

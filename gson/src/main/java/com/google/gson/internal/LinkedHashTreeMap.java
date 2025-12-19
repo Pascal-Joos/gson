79eb52a4ac51e92a29876d3036b6454314909cce
Pascal-Joos <pascal.joos@outlook.de>
2025-12-19 13:41:05 +0000

    fix: 6 - ASSIGN_FIELD_NULLABLE - e.next = e.prev = null; - assigning @Nullable expression to @NonNull field

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -17,6 +17,7 @@
 
 package com.google.gson.internal;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.ObjectStreamException;
 import java.io.Serializable;
 import java.util.AbstractMap;
@@ -115,7 +116,7 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
     // Clear all links to help GC
     Node<K, V> header = this.header;
     for (Node<K, V> e = header.next; e != header; ) {
-      Node<K, V> next = e.next;
+      Node<K, V> next = Nullability.castToNonnull(e).next;
       e.next = e.prev = null;
       e = next;
     }
@@ -252,7 +253,7 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
   void removeInternal(Node<K, V> node, boolean unlink) {
     if (unlink) {
       node.prev.next = node.next;
-      node.next.prev = node.prev;
+      Nullability.castToNonnull(node.next).prev = node.prev;
       node.next = node.prev = null; // Help the GC (for performance)
     }
 
@@ -471,7 +472,7 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
     @Nullable Node<K, V> parent;
     @Nullable Node<K, V> left;
     @Nullable Node<K, V> right;
-    Node<K, V> next;
+    @Nullable Node<K, V> next;
     @Nullable Node<K, V> prev;
     @Nullable final K key;
     final int hash;
@@ -761,7 +762,7 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
   }
 
   private abstract class LinkedTreeMapIterator<T> implements Iterator<T> {
-    Node<K, V> next = header.next;
+    @Nullable Node<K, V> next = header.next;
     @Nullable Node<K, V> lastReturned = null;
     int expectedModCount = modCount;
 

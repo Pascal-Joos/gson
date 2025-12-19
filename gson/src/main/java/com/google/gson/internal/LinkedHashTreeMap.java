d9445b6541088cf856bb5136e2f9afc631e874da
Pascal-Joos <pascal.joos@outlook.de>
2025-12-19 15:18:28 +0000

    Revert "fix: 6 - ASSIGN_FIELD_NULLABLE - e.next = e.prev = null; - assigning @Nullable expression to @NonNull field"
    This reverts commit 07db0aa38e45ffad980484136b810dda33449eba.

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -17,7 +17,6 @@
 
 package com.google.gson.internal;
 
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.ObjectStreamException;
 import java.io.Serializable;
 import java.util.AbstractMap;
@@ -115,7 +114,7 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
 
     // Clear all links to help GC
     Node<K, V> header = this.header;
-    for (Node<K, V> e = header.next; e != null && e != header; ) {
+    for (Node<K, V> e = header.next; e != header; ) {
       Node<K, V> next = e.next;
       e.next = e.prev = null;
       e = next;
@@ -252,12 +251,8 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
    */
   void removeInternal(Node<K, V> node, boolean unlink) {
     if (unlink) {
-      if (node.prev != null) {
-        node.prev.next = node.next;
-      }
-      if (node.next != null) {
-        Nullability.castToNonnull(node.next).prev = node.prev;
-      }
+      node.prev.next = node.next;
+      node.next.prev = node.prev;
       node.next = node.prev = null; // Help the GC (for performance)
     }
 
@@ -476,7 +471,7 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
     @Nullable Node<K, V> parent;
     @Nullable Node<K, V> left;
     @Nullable Node<K, V> right;
-    @Nullable Node<K, V> next;
+    Node<K, V> next;
     @Nullable Node<K, V> prev;
     @Nullable final K key;
     final int hash;
@@ -766,7 +761,7 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
   }
 
   private abstract class LinkedTreeMapIterator<T> implements Iterator<T> {
-    @Nullable Node<K, V> next = header.next;
+    Node<K, V> next = header.next;
     @Nullable Node<K, V> lastReturned = null;
     int expectedModCount = modCount;
 

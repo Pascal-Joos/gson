aa6a2df3b6cabb3e304810004195c841bb984845
Pascal-Joos <pascal.joos@outlook.de>
2025-12-19 11:01:44 +0000

    fix: 23 - DEREFERENCE_NULLABLE - Node<K, V> rightLeft = right.left; - dereferenced expression right is @Nullable

--- gson/src/main/java/com/google/gson/internal/LinkedTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedTreeMap.java
@@ -17,6 +17,7 @@
 
 package com.google.gson.internal;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.ObjectStreamException;
 import java.io.Serializable;
 import java.util.AbstractMap;
@@ -325,8 +326,8 @@ public final class LinkedTreeMap<K, V> extends AbstractMap<K, V> implements Seri
 
       int delta = leftHeight - rightHeight;
       if (delta == -2) {
-        Node<K, V> rightLeft = right.left;
-        Node<K, V> rightRight = right.right;
+        Node<K, V> rightLeft = Nullability.castToNonnull(right).left;
+        Node<K, V> rightRight = Nullability.castToNonnull(right).right;
         int rightRightHeight = rightRight != null ? rightRight.height : 0;
         int rightLeftHeight = rightLeft != null ? rightLeft.height : 0;
 
@@ -335,7 +336,7 @@ public final class LinkedTreeMap<K, V> extends AbstractMap<K, V> implements Seri
           rotateLeft(node); // AVL right right
         } else {
           assert (rightDelta == 1);
-          rotateRight(right); // AVL right left
+          rotateRight(Nullability.castToNonnull(right)); // AVL right left
           rotateLeft(node);
         }
         if (insert) {

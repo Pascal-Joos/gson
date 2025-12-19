68681d13f1a7f2331896acaf35f546b90a75a4ea
Pascal-Joos <pascal.joos@outlook.de>
2025-12-19 13:42:09 +0000

    fix: 8 - DEREFERENCE_NULLABLE - Node<K, V> leftLeft = left.left; - dereferenced expression left is @Nullable

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -17,6 +17,7 @@
 
 package com.google.gson.internal;
 
+import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.ObjectStreamException;
 import java.io.Serializable;
 import java.util.AbstractMap;
@@ -369,8 +370,8 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
         }
 
       } else if (delta == 2) {
-        Node<K, V> leftLeft = left.left;
-        Node<K, V> leftRight = left.right;
+        Node<K, V> leftLeft = Nullability.castToNonnull(left).left;
+        Node<K, V> leftRight = Nullability.castToNonnull(left).right;
         int leftRightHeight = leftRight != null ? leftRight.height : 0;
         int leftLeftHeight = leftLeft != null ? leftLeft.height : 0;
 
@@ -379,7 +380,7 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
           rotateRight(node); // AVL left left
         } else {
           assert (leftDelta == -1);
-          rotateLeft(left); // AVL left right
+          rotateLeft(Nullability.castToNonnull(left)); // AVL left right
           rotateRight(node);
         }
         if (insert) {

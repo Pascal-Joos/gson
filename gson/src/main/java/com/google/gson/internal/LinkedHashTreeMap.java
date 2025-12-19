8c3949ea2d5ca8af506bfbd0dac1fe9be801a675
Pascal-Joos <pascal.joos@outlook.de>
2025-12-19 13:47:33 +0000

    Revert "fix: 13 - DEREFERENCE_NULLABLE - Node<K, V> pivotLeft = pivot.left; - dereferenced expression pivot is @Nullable"
    This reverts commit cec98b50883d3b7373b015ea94ff1c16c7a6d24d.

--- gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
+++ gson/src/main/java/com/google/gson/internal/LinkedHashTreeMap.java
@@ -723,42 +723,24 @@ public final class LinkedHashTreeMap<K, V> extends AbstractMap<K, V> implements 
        */
       for (int scale = 4; (size & scale - 1) == scale - 1; scale *= 2) {
         if (leavesSkipped == 0) {
+          // Pop right, center and left, then make center the top of the stack.
           Node<K, V> right = stack;
-          if (right == null) {
-            break;
-          }
           Node<K, V> center = right.parent;
-          if (center == null) {
-            break;
-          }
           Node<K, V> left = center.parent;
-          if (left == null) {
-            break;
-          }
-          Node<K, V> centerParent = left.parent;
-          if (centerParent == null) {
-            break;
-          }
-          if (centerParent.parent == null) {
-            break;
-          }
-          center.parent = centerParent.parent;
-          stack = center.parent;
+          center.parent = left.parent;
+          stack = center;
+          // Construct a tree.
           center.left = left;
           center.right = right;
           center.height = right.height + 1;
           left.parent = center;
           right.parent = center;
         } else if (leavesSkipped == 1) {
+          // Pop right and center, then make center the top of the stack.
           Node<K, V> right = stack;
-          if (right == null) {
-            break;
-          }
           Node<K, V> center = right.parent;
-          if (center == null) {
-            break;
-          }
           stack = center;
+          // Construct a tree with no left child.
           center.right = right;
           center.height = right.height + 1;
           right.parent = center;

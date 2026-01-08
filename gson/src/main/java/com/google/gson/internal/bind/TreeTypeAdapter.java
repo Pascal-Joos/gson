d220b79b4dbffc45ad69c56a5c768d3f91402a06
Pascal-Joos <pascal.joos@outlook.de>
2026-01-08 08:51:40 +0000

    Revert "fix: 29 - DEREFERENCE_NULLABLE - : hierarchyType.isAssignableFrom(type.getRawType()); - dereferenced expression hierarchyType is @Nullable"
    This reverts commit a548a7f0b4140ecef27630dd0f0ecd02992c93d0.

--- gson/src/main/java/com/google/gson/internal/bind/TreeTypeAdapter.java
+++ gson/src/main/java/com/google/gson/internal/bind/TreeTypeAdapter.java
@@ -30,7 +30,6 @@ import com.google.gson.internal.Streams;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
-import edu.ucr.cs.riple.annotator.util.Nullability;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import javax.annotation.Nullable;
@@ -146,7 +145,7 @@ public final class TreeTypeAdapter<T> extends TypeAdapter<T> {
       boolean matches =
           exactType != null
               ? exactType.equals(type) || matchRawType && exactType.getType() == type.getRawType()
-              : Nullability.castToNonnull(hierarchyType).isAssignableFrom(type.getRawType());
+              : hierarchyType.isAssignableFrom(type.getRawType());
       return matches
           ? new TreeTypeAdapter<T>(
               (JsonSerializer<T>) serializer, (JsonDeserializer<T>) deserializer, gson, type, this)
